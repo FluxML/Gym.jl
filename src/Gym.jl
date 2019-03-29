f0f118ebd3b2074c88781ee3101ae6cfe7b50ed7
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-03-29 15:09:52 +0530

    Rewrote CartPoleEnv based on the created classes and updated main module

--- src/Gym.jl
+++ src/Gym.jl
@@ -7,11 +7,17 @@ import Base.show
 
 export step!, reset!, Ctx, render
 
+#Spaces
+include("Spaces/Space.jl")
+using .Space
+export sample, contains,
+    Box, Discrete, TupleSpace, DictSpace, MultiBinary, MultiDiscrete
 #Environments
-export CartPoleEnv, PendulumEnv, Continuous_MountainCarEnv
+export CartPoleEnv, PendulumEnv, Continuous_MountainCarEnv, CartPoleEnv1
 
 include("vis/utils.jl")
 include("CartPole.jl")
+include("CartPole-v1.1.jl")
 include("Pendulum.jl")
 include("Continuous-MountainCar.jl")
 
