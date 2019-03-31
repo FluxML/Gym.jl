d9a97f7762a60db5f88f5c740179cf3bed7001b9
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-03-31 11:49:51 +0530

    Switched to a BitArray and modified contains

--- src/Spaces/multi-binary.jl
+++ src/Spaces/multi-binary.jl
@@ -1,16 +1,15 @@
 
 #TODO: seed, copy
-boolor(x::Bool, y::Bool) = x || y
 
 mutable struct MultiBinary <: AbstractSpace
     n::Int
     dtype::DataType
     shape::Tuple
-    MultiBinary(n::Int) = new(n, Int, (n, ))
+    MultiBinary(n::Int) = new(n, BitArray{1}, (n, ))
 end
 
-sample(self::MultiBinary) = (self.dtype).(rand(0:1, self.n))
+sample(self::MultiBinary) = (self.dtype)(rand(0:1, self.n))
 
-contains(self::MultiBinary, x) = all(boolor.(x .== 0, x .== 1))
+contains(self::MultiBinary, x) = all((x .== 0) .| (x .== 1))
 
-Base.:(==)(self::MultiBinary, other::MultiBinary) = return self.n == other.n
+Base.:(==)(self::MultiBinary, other::MultiBinary) = self.n == other.n
