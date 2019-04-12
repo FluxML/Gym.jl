93124a263374540cfca94cc46e4e39dfb73796d6
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-04-12 17:56:48 +0530

    Registry added to the Gym and updated examples.

--- examples/CartPole/DQN.jl
+++ examples/CartPole/DQN.jl
@@ -1,4 +1,4 @@
-using Flux, Gym
+using Flux, .Gym
 using Flux.Optimise: Optimiser
 using Flux.Tracker: data
 using Statistics: mean
@@ -8,7 +8,7 @@ using Printf
 #using CuArrays
 
 # Load game environment
-env = CartPoleEnv()
+env = make("CartPole")
 reset!(env)
 
 # ----------------------------- Parameters -------------------------------------
--- src/Gym.jl
+++ src/Gym.jl
@@ -2,24 +2,17 @@ module Gym
 
 using Flux
 using Flux.Tracker
-
-#import Base.show
+using Reexport
 
 #Spaces
 include("Spaces/Space.jl")
-using .Space
-export sample,
-    Box, Discrete, TupleSpace, DictSpace, MultiBinary, MultiDiscrete
-
-#Environments
-#export CartPoleEnv, PendulumEnv, Continuous_MountainCarEnv
+@reexport using .Space
 
 # Renderer
 include("Envs/vis/utils.jl")
 export Ctx
 
-#include("Envs/classic_control/CartPole.jl")
-#include("Envs/classic_control/Pendulum.jl")
-#include("Envs/classic_control/Continuous-MountainCar.jl")
-
+include("Envs/registry.jl")
+export make, register,        # Registry functions
+       reset!, step!, render  # Environment interaction functions
 end #module
