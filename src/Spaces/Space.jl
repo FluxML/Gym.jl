42203e7e03c33a1c483ddb2f6bed7d5af526052c
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-06-19 15:38:37 +0530

    added Zygote.@nograd

--- src/Spaces/Space.jl
+++ src/Spaces/Space.jl
@@ -1,9 +1,10 @@
 module Space
 
+using Zygote: @nograd
+
 export sample
 
 abstract type AbstractSpace end
-
 
 include("box.jl")
 include("discrete.jl")
@@ -12,6 +13,7 @@ include("dict-space.jl")
 include("multi-binary.jl")
 include("multi-discrete.jl")
 
+@nograd contains
 Base.in(x, space_obj::AbstractSpace) = contains(x, space_obj)
 Base.size(space_obj::AbstractSpace) = space_obj.shape
 end #module
